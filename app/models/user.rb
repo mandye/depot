67c65826f87606b4ab616575a0b0bb4430aac395
Ye Liang <mandye@ye-liangs-mac-pro.local>
2009-05-21 02:02:57 +0800

    really finish Task F

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,12 @@ require 'digest/sha1'
 class User < ActiveRecord::Base
   validates_presence_of :name
   validates_uniqueness_of :name
-  
+
   attr_accessor :password_confirmation
   validates_confirmation_of :password
-  
+
   validate :password_non_blank
-  
+
   def self.authenticate(name, password)
     user = self.find_by_name(name)
     if user
@@ -19,27 +19,33 @@ class User < ActiveRecord::Base
     end
     user
   end
-  
+
+  def after_destroy
+    if User.count.zero?
+      raise "Can't delete last user"
+    end
+  end
+
   def password
     @password
   end
-  
+
   def password=(pwd)
     @password = pwd
     return if pwd.blank?
     create_new_salt
     self.hashed_password = User.encrypted_password(self.password, self.salt)
   end
-  
-private
+
+  private
   def password_non_blank
     errors.add(:password, 'Missing password') if hashed_password.blank?
   end
-  
+
   def create_new_salt
     self.salt = self.object_id.to_s + rand.to_s
   end
-  
+
   def self.encrypted_password(password, salt)
     string_to_hash = password + 'wibble' + salt
     Digest::SHA1::hexdigest(string_to_hash)
